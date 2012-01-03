667e97fb365f95841765bca43082fba6b99f6850
dave <dave@faith.mag-sol.com>
2012-01-03 21:53:33 +0000

    first commit

---
+++ lib/Book/Result/Book.pm
@@ -0,0 +1,145 @@
+package Book::Result::Book;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 NAME
+
+Book::Result::Book
+
+=cut
+
+__PACKAGE__->table("book");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 isbn
+
+  data_type: 'char'
+  is_nullable: 1
+  size: 10
+
+=head2 author
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 title
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 250
+
+=head2 started
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  is_nullable: 1
+
+=head2 ended
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  is_nullable: 1
+
+=head2 image_url
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 250
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "isbn",
+  { data_type => "char", is_nullable => 1, size => 10 },
+  "author",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "title",
+  { data_type => "varchar", is_nullable => 1, size => 250 },
+  "started",
+  {
+    data_type => "datetime",
+    "datetime_undef_if_invalid" => 1,
+    is_nullable => 1,
+  },
+  "ended",
+  {
+    data_type => "datetime",
+    "datetime_undef_if_invalid" => 1,
+    is_nullable => 1,
+  },
+  "image_url",
+  { data_type => "varchar", is_nullable => 1, size => 250 },
+);
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 author
+
+Type: belongs_to
+
+Related object: L<Book::Result::Author>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "author",
+  "Book::Result::Author",
+  { id => "author" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-09-06 19:50:03
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:EEN+CRZ5uZJm4AMOTh9UIw
+
+sub reading {
+  my $self = shift;
+
+  return $self->search({
+    started => { '!=' => undef },
+    ended => undef,
+  });
+}
+
+sub read {
+  my $self = shift;
+
+  return $self->search({
+    ended => { '!=' => undef },
+  });
+}
+
+sub to_read {
+  my $self = shift;
+
+  return $self->search({
+    started => undef,
+  });
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/BookWeb.pm
@@ -0,0 +1,133 @@
+package BookWeb;
+use Dancer ':syntax';
+use Dancer::Plugin::DBIC;
+use Dancer::Session;
+
+use Net::Amazon;
+use DateTime;
+
+our $VERSION = '0.1';
+
+my @public_paths = qw[/ /login /search];
+my %public_path = map { $_ => 1 } @public_paths;
+
+hook before => sub {
+    if (! session('logged_in') and ! $public_path{request->path_info}) {
+        var requested_path => request->path_info;
+        request->path_info('/login');
+    }
+};
+
+get '/' => sub {
+    my $books_rs = schema->resultset('Book');
+    
+    template 'index', {
+        reading => [ $books_rs->reading ],
+        read    => [ $books_rs->read ],
+        to_read => [ $books_rs->to_read ],
+        logged  => session 'logged_in',
+    };
+};
+
+get '/start/:isbn' => sub {
+    my $books_rs = schema->resultset('Book');
+    my $book = $books_rs->find({ isbn => param('isbn')});
+
+    if ($book) {
+        $book->update({started => DateTime->now});
+    }
+
+    return redirect '/';
+};
+
+get '/end/:isbn' => sub {
+    my $books_rs = schema->resultset('Book');
+    my $book = $books_rs->find({ isbn => param('isbn')});
+
+    if ($book) {
+        $book->update({ended => DateTime->now});
+    }
+
+    return redirect '/';
+};
+
+get '/add/:isbn' => sub {
+    my $author_rs = schema->resultset('Author');
+
+    my $amz = get_amazon();
+
+    # Search for the book at Amazon
+    my $resp = $amz->search(asin => param('isbn'));
+
+    unless ($resp->is_success) {
+        die 'Error: ', $resp->message;
+    }
+
+    my $book = $resp->properties;
+    my $title = $book->ProductName;
+    my $author_name = ($book->authors)[0];
+    my $imgurl = $book->ImageUrlMedium;
+
+    # Find or create the author
+    my $author = $author_rs->find_or_create({
+        name => $author_name,
+    });
+
+    # Add the book to the author
+    $author->add_to_books({
+        isbn => param('isbn'),
+        title => $title,
+        image_url => $imgurl,
+    });
+
+    return redirect '/';
+};
+
+post '/search' => sub {
+    my $amz = get_amazon();
+   
+    my $resp = $amz->search(
+        keyword => param('search'),
+        mode => 'books',
+    );
+
+    my %data;
+    $data{search} = param('search');
+    if ($resp->is_success) {
+        $data{books} = [ $resp->properties ];
+    } else {
+        $data{error} = $resp->message;
+    }
+
+    template 'results', \%data;
+};
+
+get '/login' => sub {
+    template 'login', { path => vars->{requested_path } };  
+};
+
+post '/login' => sub {
+    if (params->{user} eq 'reader' && params->{pass} eq 'letmein') {
+        session 'logged_in' => 1;
+    }
+
+    redirect  params->{path} || '/';
+};
+
+get '/logout' => sub {
+    session 'logged_in' => 0;
+    
+    redirect '/';
+};
+
+sub get_amazon {
+     return Net::Amazon->new(
+        token => $ENV{AMAZON_KEY},
+        secret_key => $ENV{AMAZON_SECRET},
+        associate_tag => $ENV{AMAZON_ASSTAG},
+        locale => 'uk',
+    ) or die "Cannot connect to Amazon\n";
+}
+
+
+true;
